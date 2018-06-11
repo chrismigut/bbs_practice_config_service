4e436c307ad56e2523982894b9d5d0446a095894
Chris Migut <cmiguthere@yahoo.com>
2018-06-10 22:47:37 -0400

    inital commit

---
+++ spring_cloud_config_server/src/main/java/binarybyte/services/spring_cloud_config_server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package binarybyte.services.spring_cloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
